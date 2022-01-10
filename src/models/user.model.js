11d271c26e766727724a20b1cc1fd78b49fa64f6
MeghnadShirole <shirolemeghnad10@gmail.com>
2022-01-10 21:17:09 +0530

    [Meghnad]_Added: Route and Controller function for User Registration

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,14 +1,28 @@
 import { Schema, model } from 'mongoose';
 
-const userSchema = new Schema(
-  {
-    name: {
-      type: String
-    }
-  },
-  {
-    timestamps: true
-  }
-);
+const userSchema = new Schema({
+        firstname: {
+            type: String,
+            required: true
+        },
+        lastname: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    },
 
-export default model('User', userSchema);
+    {
+        timestamps: true,
+        versionKey: false
+    }
+)
+export default model('User', userSchema)
