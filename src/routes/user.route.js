eb8bef322e83858d752160d2f2955fcdf80f850e
MeghnadShirole <shirolemeghnad10@gmail.com>
2022-01-10 21:19:56 +0530

    [Meghnad]_Added: Validations for User Registration

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import * as userController from '../controllers/user.controller';
+import { newUserValidator } from '../validators/user.validator';
 
 const router = express.Router();
 
-router.post('/registration', userController.registration)
+router.post('/registration', newUserValidator, userController.registration)
 
 export default router;
