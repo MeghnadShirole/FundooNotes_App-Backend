e89085309ea851c77fc885d585b36e99d7383827
MeghnadShirole <shirolemeghnad10@gmail.com>
2022-01-11 17:55:51 +0530

    [Meghnad]_Added: Validations for new note

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as noteController from '../controllers/note.controller.js';
+import { newNoteValidator } from '../validators/note.validator.js';
 
 const router = express.Router();
 
-router.post('/createNote', noteController.newNote)
+router.post('/createNote', newNoteValidator, noteController.newNote)
