41b5e21a826cdf6fd5967cc6bb742e2dcfb272c6
MeghnadShirole <shirolemeghnad10@gmail.com>
2022-01-11 18:02:31 +0530

    [Meghnad]_Added: Authentication for creating a new note

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+
+/**
+ * Middleware to authenticate if note has a valid Authorization token
+ *
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ */
+
+export const userAuth = (req, res, next) => {
+    const requestToken = req.headers['token'];
+    if (requestToken) {
+        /**
+         * @description:verifies secret and checks expression
+         **/
+        jwt.verify(requestToken, process.env.SECRET_KEY, (err, decode) => {
+            if (err) {
+                return res.status(401).send({
+                    status: false,
+                    message: 'Unauthorised access, please provide valid token!'
+                });
+            } else {
+                req.userData = decode;
+                req.body['userId'] = decode._id;
+                next();
+            }
+        });
+    } else {
+        /**
+         * @description:if there is no token return an error
+         **/
+        return res.send({
+            status: false,
+            message: 'No token provided!!'
+        });
+    }
+}
--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import * as noteController from '../controllers/note.controller.js';
 import { newNoteValidator } from '../validators/note.validator.js';
+import { userAuth } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/createNote', newNoteValidator, noteController.newNote)
+router.post('/createNote', userAuth, newNoteValidator, noteController.newNote)
