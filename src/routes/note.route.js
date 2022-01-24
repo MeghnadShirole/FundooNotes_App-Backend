da76b9bd91aaac4e1185888a3492462e433abc69
MeghnadShirole <shirolemeghnad10@gmail.com>
2022-01-24 13:18:20 +0530

    [Meghnad]_Refactored: refactored for standard routes

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -6,16 +6,22 @@ import { userAuth } from '../middlewares/auth.middleware';
 const router = express.Router();
 
 //route for creating a note
-router.post('/createNote', newNoteValidator, userAuth, noteController.newNote)
+router.post('', newNoteValidator, userAuth, noteController.newNote)
 
 //route to get all notes
-router.get('/getAllNotes', userAuth, noteController.getAllNotes);
+router.get('', userAuth, redis.redis_data, noteController.getAllNotes);
+
+//route to get all archieved notes
+router.get('/archievedNotes', userAuth, noteController.getArchievedNotes);
+
+//route to get all trashed notes
+router.get('/trashedNotes', userAuth, noteController.getTrashedNotes);
 
 //route to get a single note by their note id
-router.get('/getNote/:_id', userAuth, noteController.getNote);
+router.get('/:_id', userAuth, redis.redis_single_note, noteController.getNote);
 
 //route to update a single note by their note id
-router.put('/update/:_id', userAuth, noteController.updateNote);
+router.put('/:_id', newNoteValidator, userAuth, noteController.updateNote);
 
 //route to archieve a single note by their note id
 router.put('/archieve/:_id', userAuth, noteController.archieveNote);
@@ -24,11 +30,6 @@ router.put('/archieve/:_id', userAuth, noteController.archieveNote);
 router.put('/trash/:_id', userAuth, noteController.trashNote);
 
 //route to delete a single note by their note id
-router.delete('/delete/:_id', userAuth, noteController.deleteNote);
+router.delete('/:_id', userAuth, noteController.deleteNote);
 
-//route to get all archieved notes
-router.get('/archievedNotes', userAuth, noteController.getArchievedNotes);
-
-//route to get all trashed notes
-router.get('/trashedNotes', userAuth, noteController.getTrashedNotes);
 export default router;
